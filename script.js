acb3e8bfa33a0bd44a007daea8630f207f873396
FaizanHashmi3 <96041026+FaizanHashmi3@users.noreply.github.com>
2023-04-15 13:23:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  
+  function PrintDeveloperbyMap() {
+    //Write your code here , just console.log
+    arr.map(function(value){
+      if(value.profession=="developer"){
+        console.log(value);
+      }
+    })
+  }
+  
+  function PrintDeveloperbyForEach() {
+    //Write your code here , just console.log
+    arr.forEach(function(value){
+      if(value.profession=="developer"){
+        console.log(value);
+      }
+  });
+  }
+  
+  function addData() {
+    //Write your code here, just console.log
+    let newobj = {id:4,name:"susan",age:"20",profession:"intern"};
+    arr.push(newobj)
+    console.log(arr);
+  }
+  
+  function removeAdmin() {
+    //Write your code here, just console.log
+    let filterArr= arr.filter((arr)=>arr.profession!="admin");
+    console.log(filterArr);
+  }
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+    let newArr = 
+    [
+        { id: 5, name: "Hashmi", age: "19", profession: "developer"},
+        { id: 6, name: "Faizan", age: "21", profession: "developer" },
+        { id: 7, name: "Fahad", age: "22", profession: "developer" }
+
+    ];
+    let concatArr = arr.concat(newArr);
+    console.log(concatArr);
+  }
